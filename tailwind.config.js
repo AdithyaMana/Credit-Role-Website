e42939ce9099dbad3f3cc4e5e13a17768b3e86fe
Adithya <adithyamana5517@gmail.com>
2026-03-03 11:15:43 +0530

    feat(phase-1): Setup Proper Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        strategy: '#A5B4FC', // Indigo 300
+        research: '#5EEAD4', // Teal 300
+        infrastructure: '#CBD5E1', // Slate 300
+        dissemination: '#FDBA74', // Orange 300
+      }
+    },
+  },
+  plugins: [],
+}
