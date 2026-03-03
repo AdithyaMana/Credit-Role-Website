2d4b94cb0b5f4d1d07ea64cdac4ddfa4bba80839
Adithya <adithyamana5517@gmail.com>
2026-03-03 15:11:41 +0530

    fix: Add z-10 to mobile wrapper to block background dots from bleeding through

--- App.tsx
+++ App.tsx
@@ -136,7 +136,7 @@ const App: React.FC = () => {
         )}
       </div>
 
-      <div className="lg:hidden h-full">
+      <div className="lg:hidden h-full relative z-10">
         <MobileLayout
           onOpenShowcase={() => setIsShowcaseOpen(true)}
           onOpenAbout={() => setIsAboutOpen(true)}
